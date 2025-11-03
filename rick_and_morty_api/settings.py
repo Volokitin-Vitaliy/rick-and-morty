2cf1e34905200f7e5f5cd9a76c6a624db567e36a
Fibonacci <volokitin.vitaliy@icloud.com>
2025-11-03 10:09:58 +0200

    Added Celery with Django-celery-beat schedular

--- rick_and_morty_api/settings.py
+++ rick_and_morty_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "drf_spectacular",
     "rest_framework",
     "characters",
+    "django_celery_beat",
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,9 @@ SPECTACULAR_SETTINGS = {
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
 }
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_TIMEZONE = "Europe/Kiev"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
