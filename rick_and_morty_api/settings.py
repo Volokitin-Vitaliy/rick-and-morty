8aa646ca68759d2a3508fc855f52a0d9483f5299
Fibonacci <volokitin.vitaliy@icloud.com>
2025-11-05 07:37:22 +0200

    Added postgres DB in settings.py

--- rick_and_morty_api/settings.py
+++ rick_and_morty_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,8 +79,12 @@ WSGI_APPLICATION = "rick_and_morty_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "rick_and_morty",
+        "USER": "rick_and_morty_user",
+        "PASSWORD": "rick_and_morty_user_password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
